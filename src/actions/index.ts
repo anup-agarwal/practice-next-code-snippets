cfd679d98f3fa4f2c41a1066111389133acab5cc
Anup Agarwal <anupagarwal@Anups-MacBook-Pro.local>
2025-05-28 00:40:15 +0530

    added useActionState

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,21 +1,43 @@
-'use server';
+"use server"
 
-import { redirect } from 'next/navigation';
-import { db } from '@/db';
+import { redirect } from "next/navigation"
+import { db } from "@/db"
 
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({
     where: { id },
     data: { code },
-  });
-
-  redirect(`/snippets/${id}`);
+  })
+  redirect(`/snippets/${id}`)
 }
 
 export async function deleteSnippet(id: number) {
   await db.snippet.delete({
     where: { id },
-  });
+  })
+  redirect("/")
+}
 
-  redirect('/');
+export async function createSnippet(
+  formaState: { msg: string },
+  formData: FormData
+) {
+  const title = formData.get("title")
+  const code = formData.get("code")
+
+  if (typeof title !== "string" || title.length < 3) {
+    return { msg: "Title must be longer" }
+  }
+  if (typeof code !== "string" || title.length < 10) {
+    return { msg: "Code must be longer" }
+  }
+
+  await db.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  })
+
+  redirect("/")
 }
--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client"
 
-import type { Snippet } from '@prisma/client';
-import Editor from '@monaco-editor/react';
-import { useState } from 'react';
-import * as actions from '@/actions';
+import type { Snippet } from "@prisma/client"
+import Editor from "@monaco-editor/react"
+import { useState } from "react"
+import * as actions from "@/actions"
 
 interface SnippetEditFormProps {
-  snippet: Snippet;
+  snippet: Snippet
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-  const [code, setCode] = useState(snippet.code);
+  const [code, setCode] = useState(snippet.code)
 
-  const handleEditorChange = (value: string = '') => {
-    setCode(value);
-  };
+  const handleEditorChange = (value: string = "") => {
+    setCode(value)
+  }
 
-  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code)
 
   return (
     <div>
@@ -34,5 +34,5 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         </button>
       </form>
     </div>
-  );
+  )
 }
